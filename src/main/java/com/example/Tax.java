3b34dc37a6d2f7f8359536042451eee388b056e7
Kevin <kevin_sbdiv@gmail.com>
2021-06-09 14:16:09 +0900

    rev_002

--- src/main/java/com/example/Tax.java
+++ src/main/java/com/example/Tax.java
@@ -1,10 +1,11 @@
 package com.example;
 
 public class Tax {
-    //合計金額クラスを生成する？他のクラスとどうやってつながるんやろ
+    //TODO 合計金額クラスを生成する？他のクラスとどうやってつながるんやろ
     int beforeTax;
     int afterTax;
 
+    // Here is the constructor
     public Tax(int beforeTax) {
         this.beforeTax = beforeTax;
         if (beforeTax < 0) {
@@ -12,9 +13,12 @@ public class Tax {
         }
     }
 
+    //calculating the sales tax here.
     public int getTaxIncludedPrice(int beforeTax) {
-        double temp = (double)beforeTax * 1.08;  // Sales tax = 8%
-        int afterTax = (int)temp;
+        final double taxRate = 1.08;    // Sales tax = 8%
+        double temp = (double)beforeTax * taxRate;
+        int applytax = (int)temp;       //apply tax
+        int afterTax = Math.round(applytax); //rounded up to the nearest whole number
         return afterTax;
     }
 
