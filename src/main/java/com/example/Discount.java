c7d69bf4e8ef8ad422a607e72e4c94066e0a333f
Shuto Horie <shuto.horie.sb@gmail.com>
2021-06-09 14:23:47 +0900

    Discount class was implemented.

--- src/main/java/com/example/Discount.java
+++ src/main/java/com/example/Discount.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import java.util.Map;
+
 public class Discount {
-    public int discount;
 
-
+    public int calcDiscount(Map<Product, Integer> purchasedProductMap) {
+        int numApple = purchasedProductMap.get(Product.APPLE);
+        int discountValue = 0;
+        discountValue -= (numApple / 3)*20;
+        return discountValue;
+    }
 }
