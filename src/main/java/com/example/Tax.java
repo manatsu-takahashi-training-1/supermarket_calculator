b63353d1357c79434aeadbee1e92cff3b2193e67
Kevin <kevin_sbdiv@gmail.com>
2021-06-09 14:51:20 +0900

    rev_003

--- src/main/java/com/example/Tax.java
+++ src/main/java/com/example/Tax.java
@@ -3,7 +3,6 @@ package com.example;
 public class Tax {
     //TODO 合計金額クラスを生成する？他のクラスとどうやってつながるんやろ
     int beforeTax;
-    int afterTax;
 
     // Here is the constructor
     public Tax(int beforeTax) {
@@ -15,10 +14,9 @@ public class Tax {
 
     //calculating the sales tax here.
     public int getTaxIncludedPrice(int beforeTax) {
-        final double taxRate = 1.08;    // Sales tax = 8%
-        double temp = (double)beforeTax * taxRate;
-        int applytax = (int)temp;       //apply tax
-        int afterTax = Math.round(applytax); //rounded up to the nearest whole number
+        final int taxRate = 108;    // Sales tax = 8%
+        int temp = beforeTax * taxRate / 100;
+        int afterTax = temp;       //apply tax
         return afterTax;
     }
 
