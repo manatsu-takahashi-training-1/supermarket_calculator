9227157ac14b90ca1a41c751a369e0069d5f10ce
Manatsu Takahashi <ynn@your-diary.com>
2021-06-09 16:19:50 +0900

    Added `Main.java`.

--- src/main/java/com/example/CreateProductMap.java
+++ src/main/java/com/example/CreateProductMap.java
@@ -19,39 +19,16 @@ public class CreateProductMap {
         public int quantity;
     }
 
-//     public Map<Product,Integer> create(String json_string) {
-    public static void main(String[] args) {
+    public Map<Product,Integer> create(Map<String,Integer> purchaseMap) {
 
-        Gson gson = new Gson();
+        SearchProduct s = new SearchProduct();
 
-        List<A> l = gson.fromJson("[{\"name\": \"Apple\", \"quantity\": 3}, {\"name\": \"Orange\", \"quantity\": 2}]", List<A>.class);
+        for (Map.Entry<String, Integer> e : purchaseMap.entrySet()){
+            Product product = s.search(e.getKey());
+            productMap.put(product,e.getValue());
+        }
 
-//         A a = gson.fromJson("{\"name\": \"Apple\", \"quantity\": 3}", A.class);
-//         System.out.println(a.name);
-//         System.out.println(a.quantity);
-
-//         List<A> l = gson.fromJson(, List<A>.class);
-
-//         List<String, Integer>
-
-// // Deserialization
-// int one = gson.fromJson("1", int.class);
-// Integer one = gson.fromJson("1", Integer.class);
-// Long one = gson.fromJson("1", Long.class);
-// Boolean false = gson.fromJson("false", Boolean.class);
-// String str = gson.fromJson("\"abc\"", String.class);
-// String[] anotherStr = gson.fromJson("[\"abc\"]", String[].class);
-
-//         Map<String,Integer> purchaseMap
-// 
-//         SearchProduct s = new SearchProduct();
-// 
-//         for (Map.Entry<String, Integer> e : purchaseMap.entrySet()){
-//             Product product = s.search(e.getKey());
-//             productMap.put(product,e.getValue());
-//         }
-// 
-//         return productMap;
+        return productMap;
     }
 
 }
