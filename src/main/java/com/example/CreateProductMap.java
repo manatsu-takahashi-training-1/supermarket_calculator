97b53ec7eba75a2dda02798c7660ece9cc683681
Manatsu Takahashi <ynn@your-diary.com>
2021-06-09 16:08:36 +0900

    Intermediate.

--- src/main/java/com/example/CreateProductMap.java
+++ src/main/java/com/example/CreateProductMap.java
@@ -1,7 +1,10 @@
 package com.example;
 
+import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.google.gson.Gson;
 
 public class CreateProductMap {
 
@@ -11,16 +14,44 @@ public class CreateProductMap {
         productMap = new HashMap<>();
     }
 
-    public Map<Product,Integer> create(Map<String,Integer> purchaseMap){
+    private class A {
+        public String name;
+        public int quantity;
+    }
 
-        SearchProduct s = new SearchProduct();
+//     public Map<Product,Integer> create(String json_string) {
+    public static void main(String[] args) {
 
-        for (Map.Entry<String, Integer> e : purchaseMap.entrySet()){
-            Product product = s.search(e.getKey());
-            productMap.put(product,e.getValue());
-        }
+        Gson gson = new Gson();
 
-        return productMap;
+        List<A> l = gson.fromJson("[{\"name\": \"Apple\", \"quantity\": 3}, {\"name\": \"Orange\", \"quantity\": 2}]", List<A>.class);
+
+//         A a = gson.fromJson("{\"name\": \"Apple\", \"quantity\": 3}", A.class);
+//         System.out.println(a.name);
+//         System.out.println(a.quantity);
+
+//         List<A> l = gson.fromJson(, List<A>.class);
+
+//         List<String, Integer>
+
+// // Deserialization
+// int one = gson.fromJson("1", int.class);
+// Integer one = gson.fromJson("1", Integer.class);
+// Long one = gson.fromJson("1", Long.class);
+// Boolean false = gson.fromJson("false", Boolean.class);
+// String str = gson.fromJson("\"abc\"", String.class);
+// String[] anotherStr = gson.fromJson("[\"abc\"]", String[].class);
+
+//         Map<String,Integer> purchaseMap
+// 
+//         SearchProduct s = new SearchProduct();
+// 
+//         for (Map.Entry<String, Integer> e : purchaseMap.entrySet()){
+//             Product product = s.search(e.getKey());
+//             productMap.put(product,e.getValue());
+//         }
+// 
+//         return productMap;
     }
 
 }
