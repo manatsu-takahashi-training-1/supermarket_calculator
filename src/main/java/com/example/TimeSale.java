029a8254ae70b7a0e7acfdda386f355f343ab9c9
香川博 <kagawah38@kagawahiroshinoMacBook-Pro.local>
2021-06-11 08:39:17 +0900

    makes time sales class

---
+++ src/main/java/com/example/TimeSale.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import java.util.Map;
+
+public class TimeSale {
+    public int calcSale(Map<String, Integer> purchasedProductStringMap) {
+        CreateProductMap createProductMap = new CreateProductMap();
+
+        Map<Product, Integer> purchasedProductMap = createProductMap.create(purchasedProductStringMap);
+
+        // Todo: Create Sum class when refactoring
+        int totalAmount = 0;
+        for (final Product product: purchasedProductMap.keySet()) {
+            final int quantity = purchasedProductMap.get(product);
+            totalAmount += quantity;
+        }
+
+        totalAmount = (new Tax(0)).getTaxIncludedPrice(totalAmount);
+
+        //Todo: print for debug
+        System.out.println(totalAmount);
+
+        return totalAmount;
+    }
+}
