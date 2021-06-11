7cf959e7631a43349ef811c4f1ba579bbd33d0ca
Manatsu Takahashi <ynn@your-diary.com>
2021-06-11 11:37:52 +0900

    Now the product buttons flash when on time sale.

--- src/main/java/com/gui/CalculatorGUI.java
+++ src/main/java/com/gui/CalculatorGUI.java
@@ -29,6 +29,10 @@ public class CalculatorGUI extends JFrame {
 
     public static final Color defaultBgColor = Color.gray;
 
+    private static Map<String, JButton> productButtonMap;
+
+    private static Timer flashButtonTimer;
+
     //} GUI
 
     //CUI {
@@ -49,6 +53,8 @@ public class CalculatorGUI extends JFrame {
 
         //CUI
         {
+
+            CalculatorGUI.productButtonMap = new HashMap<>();
 
             CalculatorGUI.cart = new HashMap<>();
             CalculatorGUI.subtotalEntryMap = new HashMap<>();
@@ -92,31 +98,31 @@ public class CalculatorGUI extends JFrame {
             CalculatorGUI.panel1.setLayout(new GridLayout(/* number of rows = */ 4, /* number of columns = */ 3));
             CalculatorGUI.panel1.setBackground(CalculatorGUI.defaultBgColor);
 
-            List<JButton> productButtonList = null;
             try {
-                productButtonList = List.of(
-                    new JButton("<html>りんご<br/>(100円)</html>",              IconGenerator.generateIcon("りんご")),
-                    new JButton("<html>みかん<br/>(40円)</html>",               IconGenerator.generateIcon("みかん")),
-                    new JButton("<html>ぶどう<br/>(150円)</html>",              IconGenerator.generateIcon("ぶどう")),
-                    new JButton("<html>のり弁<br/>(350円)</html>",              IconGenerator.generateIcon("のり弁")),
-                    new JButton("<html>しゃけ弁<br/>(400円)</html>",            IconGenerator.generateIcon("しゃけ弁")),
-                    new JButton("<html>タバコ<br/>(420円)</html>",              IconGenerator.generateIcon("タバコ")),
-                    new JButton("<html>メンソールタバコ<br/>(440円)</html>",    IconGenerator.generateIcon("メンソールタバコ")),
-                    new JButton("<html>ライター<br/>(100円)</html>",            IconGenerator.generateIcon("ライター")),
-                    new JButton("<html>お茶<br/>(80円)</html>",                 IconGenerator.generateIcon("お茶")),
-                    new JButton("<html>コーヒー<br/>(100円)</html>",            IconGenerator.generateIcon("コーヒー"))
-                );
+                CalculatorGUI.productButtonMap.put("りんご", new JButton("<html>りんご<br/>(100円)</html>", IconGenerator.generateIcon("りんご")));
+                CalculatorGUI.productButtonMap.put("みかん", new JButton("<html>みかん<br/>(40円)</html>", IconGenerator.generateIcon("みかん")));
+                CalculatorGUI.productButtonMap.put("ぶどう", new JButton("<html>ぶどう<br/>(150円)</html>", IconGenerator.generateIcon("ぶどう")));
+                CalculatorGUI.productButtonMap.put("のり弁", new JButton("<html>のり弁<br/>(350円)</html>", IconGenerator.generateIcon("のり弁")));
+                CalculatorGUI.productButtonMap.put("しゃけ弁", new JButton("<html>しゃけ弁<br/>(400円)</html>", IconGenerator.generateIcon("しゃけ弁")));
+                CalculatorGUI.productButtonMap.put("タバコ", new JButton("<html>タバコ<br/>(420円)</html>", IconGenerator.generateIcon("タバコ")));
+                CalculatorGUI.productButtonMap.put("メンソールタバコ", new JButton("<html>メンソールタバコ<br/>(440円)</html>", IconGenerator.generateIcon("メンソールタバコ")));
+                CalculatorGUI.productButtonMap.put("ライター", new JButton("<html>ライター<br/>(100円)</html>", IconGenerator.generateIcon("ライター")));
+                CalculatorGUI.productButtonMap.put("お茶", new JButton("<html>お茶<br/>(80円)</html>", IconGenerator.generateIcon("お茶")));
+                CalculatorGUI.productButtonMap.put("コーヒー", new JButton("<html>コーヒー<br/>(100円)</html>", IconGenerator.generateIcon("コーヒー")));
             } catch (Exception e) {
                 ;
             }
 
-            for (JButton productButton: productButtonList) {
+            final int numPureProductButton = CalculatorGUI.productButtonMap.size();
+
+            for (JButton productButton: CalculatorGUI.productButtonMap.values()) {
                 productButton.addActionListener(new ProductButtonHandler());
                 CalculatorGUI.panel1.add(productButton);
             }
 
             JButton flashHamburgerButton = new JButton("<html>光のハンバーガー<br/>(？？？円)</html>", IconGenerator.generateIcon("光のハンバーガー"));
             flashHamburgerButton.addActionListener(new FlashHamburgerButtonHandler());
+            CalculatorGUI.productButtonMap.put("光のハンバーガー", flashHamburgerButton);
             CalculatorGUI.panel1.add(flashHamburgerButton);
 
             JButton allResetButton = null;
@@ -126,19 +132,45 @@ public class CalculatorGUI extends JFrame {
                 ;
             }
             allResetButton.addActionListener(new AllResetButtonHandler());
+            CalculatorGUI.productButtonMap.put("りせっと", allResetButton);
             CalculatorGUI.panel1.add(allResetButton);
+
+            for (JButton b: CalculatorGUI.productButtonMap.values()) {
+                b.setOpaque(true); //needed to make `setBackground()` work (ref: |https://stackoverflow.com/questions/4990952/why-does-setbackground-to-jbutton-does-not-work|)
+                b.setBackground(null);
+            }
 
             //} panel1
      
             //panel2
             CalculatorGUI.panel2 = new JPanel();
-            CalculatorGUI.panel2.setLayout(new GridLayout(productButtonList.size() + 1, 1));
+            CalculatorGUI.panel2.setLayout(new GridLayout(numPureProductButton + 1, 1));
             CalculatorGUI.panel2.setBackground(CalculatorGUI.defaultBgColor);
             CalculatorGUI.panel2.add(totalAmountDisplay);
 
             CalculatorGUI.frame.add(panel1, BorderLayout.LINE_START);
             CalculatorGUI.frame.add(panel2, BorderLayout.CENTER);
      
+            ActionListener timerAction = new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    for (JButton b: CalculatorGUI.productButtonMap.values()) {
+                        final Color currentColor = b.getBackground();
+                        if (currentColor == Color.red) {
+                            b.setBackground(Color.blue);
+                        } else if (currentColor == Color.blue) {
+                            b.setBackground(Color.orange);
+                        } else if (currentColor == Color.orange) {
+                            b.setBackground(Color.green);
+                        } else {
+                            b.setBackground(Color.red);
+                        }
+                    }
+                    CalculatorGUI.redraw();
+                }
+            };
+            CalculatorGUI.flashButtonTimer = new Timer(/* 60sec / 90bpm * 1000 = */ 666, timerAction);
+            CalculatorGUI.flashButtonTimer.setRepeats(true);
+
         }
 
     }
@@ -176,10 +208,16 @@ public class CalculatorGUI extends JFrame {
     }
 
     public static void toggleTimeSale() {
+
         if (CalculatorGUI.isTimeSale) {
             CalculatorGUI.isTimeSale = false;
+            CalculatorGUI.flashButtonTimer.stop();
+            for (JButton b: CalculatorGUI.productButtonMap.values()) {
+                b.setBackground(null);
+            }
         } else {
             CalculatorGUI.isTimeSale = true;
+            flashButtonTimer.start();
         }
         BGM.toggle();
         CalculatorGUI.recalculateTotalAmountDisplay();
